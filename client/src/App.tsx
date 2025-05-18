d15cb6867e0c6b1da1677f48635541325858c852
GalacticQuasar <akashdfw@gmail.com>
2025-05-18 00:35:39 -0500

    cleanup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { ThemeProvider } from "@/components/theme-provider"
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
 import ChatInterface from '@/components/chat-interface'
 
