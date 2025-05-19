4dc3041a565acd19c67f957b2ae195c506c912b6
GalacticQuasar <akashdfw@gmail.com>
2025-05-18 19:56:08 -0500

    textarea styling

--- client/src/components/chat-interface.tsx
+++ client/src/components/chat-interface.tsx
@@ -31,7 +31,7 @@ function ChatInterface() {
       </h1>
       <div>
         <div 
-          className="m-10 rounded-2xl p-3 border-2 focus-within:border-teal-300/100 hover:border-teal-300/100 transition-all duration-300 cursor-text"
+          className="m-10 rounded-2xl p-3 border-1 focus-within:border-teal-300/100 hover:border-teal-300/100 transition-all duration-300 cursor-text"
           onClick={(e) => {
             const textarea = e.currentTarget.querySelector('textarea')
             if (textarea) {
@@ -39,8 +39,8 @@ function ChatInterface() {
             }
           }}
         >
-          <LLMTextarea className="w-180 font-mono" placeholder="How can I help you today?" value={prompt} onChange={(e) => setPrompt(e.target.value)} />
-          <div className="mt-2 flex justify-end">
+          <LLMTextarea className="w-160 font-mono" placeholder="How can I help you today?" value={prompt} onChange={(e) => setPrompt(e.target.value)} />
+          <div className="flex justify-end">
             <Button variant="outline" size="icon" className="font-semibold font-mono hover:text-teal-300" onClick={handleSend}>
               <Send className="scale-125" />
             </Button>
