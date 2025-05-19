dc67a207bd906470bfa3302b49c7a49e190e4808
GalacticQuasar <akashdfw@gmail.com>
2025-05-18 22:48:07 -0500

    new sidebar redesign

--- client/src/components/app-sidebar.tsx
+++ client/src/components/app-sidebar.tsx
@@ -52,21 +52,37 @@ export function AppSidebar() {
 
   return (
     <Sidebar collapsible="icon">
-      <SidebarHeader>
-        <SidebarTrigger className="m-0.5 scale-125 hover:text-teal-300" />
-        <Button variant="outline" className="flex items-center gap-2 text-teal-300 hover:text-teal-500">
-          <CirclePlus className="m-1 scale-125" />
-          {!isCollapsed ? <span className="">New Chat</span> : null}
-        </Button>
-      </SidebarHeader>
+
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel className="ml-1">Recent Chats</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton className="hover:bg-transparent" asChild>
+                  <a href="#">
+                    <SidebarTrigger className="scale-125 hover:text-teal-300" />
+                    <span className="ml-2 font-serif text-2xl">Nova</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton className="text-teal-300 hover:text-teal-500" asChild>
+                  <a href="#">
+                    <CirclePlus className="scale-140" />
+                    <span className="ml-2 font-mono">New Chat</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>Recent Chats</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
               <div className={`transition-opacity duration-300 ${isCollapsed ? 'opacity-0 pointer-events-none' : 'opacity-100'}`}>
                 {items.map((item) => (
-                  <SidebarMenuItem className="m-1" key={item.title}>
+                  <SidebarMenuItem className="my-0.5" key={item.title}>
                     <SidebarMenuButton asChild>
                       <a href={item.url}>
                         <span>{item.title}</span>
