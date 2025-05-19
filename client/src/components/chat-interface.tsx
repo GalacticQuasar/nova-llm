4b0f6d2c76b4ecb0cd4fa71b68b048b31c07e49d
GalacticQuasar <akashdfw@gmail.com>
2025-05-18 20:17:01 -0500

    start state

--- client/src/components/chat-interface.tsx
+++ client/src/components/chat-interface.tsx
@@ -5,12 +5,16 @@ import { Send } from "lucide-react"
 import { postChat } from '@/api/api'
 
 function ChatInterface() {
+  const [startState, setStartState] = useState(true)
   const [prompt, setPrompt] = useState("")
   const [response, setResponse] = useState("")
   const [isLoading, setIsLoading] = useState(false)
   const [isError, setIsError] = useState(false)
 
   const handleSend = async () => {
+    if (startState) {
+      setStartState(false)
+    }
     setIsLoading(true)
     try {
       const response = await postChat(prompt)
@@ -25,13 +29,14 @@ function ChatInterface() {
   
   return (
     <div className="dark text-teal-50 flex flex-col items-center justify-center min-h-screen">
-      <h1 id="welcome-message" className="text-center text-6xl font-serif flex items-center gap-4">
+      {startState && <h1 id="welcome-message" className="text-center text-6xl font-serif flex items-center gap-4">
         <img src="/Galactic-Logo.png" alt="Galactic Logo" className="h-16 w-16" />
         <span className="text-teal-300">Welcome,</span> User
-      </h1>
+      </h1>}
       <div>
         <div
           id="chat-input-container"
+          //TODO: ${startState ? 'm-10' : 'mt-auto mb-4'} 
           className={`m-10 rounded-2xl p-3 border-1 focus-within:border-teal-300/100 hover:border-teal-300/100 transition-all duration-300 cursor-text ${isLoading ? 'opacity-70 pointer-events-none' : ''}`}
           onClick={(e) => {
             const textarea = e.currentTarget.querySelector('textarea')
